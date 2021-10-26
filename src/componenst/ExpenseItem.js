718728e925068e1cd1869dfa1cd727036e84d05c
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2021-10-26 19:33:25 +0300

    Add mock data

--- src/componenst/ExpenseItem.js
+++ src/componenst/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <h2>{props.title}</h2>
+        <div>${props.amount}</div>
       </div>
     </div>
   );
