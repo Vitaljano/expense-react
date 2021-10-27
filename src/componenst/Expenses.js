a6a64a1bd112dbc18875a7481ba12f78f8a3ac44
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2021-10-27 18:50:19 +0300

    Add composition concept <children props> Card component

--- src/componenst/Expenses.js
+++ src/componenst/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 import "./Expenses.css";
 
 const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -20,7 +21,7 @@ const Expenses = (props) => {
         amount={props.expenses[2].amount}
         date={props.expenses[2].date}
       />
-    </div>
+    </Card>
   );
 };
 
