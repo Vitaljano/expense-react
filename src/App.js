60d7e2fe997a2eac07982ecddebe0f062c08fea0
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2021-10-30 13:43:41 +0300

    Rendering list of data

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,22 +18,15 @@ const Expenses = (props) => {
         selected={filterYear}
         onFilterByYear={onFilterByYearHandler}
       />
-
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
+      {props.items.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
     </Card>
   );
 };
